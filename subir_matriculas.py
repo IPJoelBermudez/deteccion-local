b3bb46e8feb35f62f6dab7ba4bcc5131ba9a35d2
= <joel.bermudez@iparkings.com>
2024-06-12 12:56:36 -0300

    Agregando funciones basicas

---
+++ subir_matriculas.py
@@ -0,0 +1,54 @@
+import socket
+import ipaddress
+
+
+estrucutraJson = {
+    "LicensePlateInfoList": [{
+        "LicensePlate": "BBB1234",
+        "listType": "whiteList",
+        "createTime": "2024-03-26T16:30:34",
+        "effectiveStartDate": "2024-03-26",
+        "effectiveTime": "2024-03-26",
+        "id": ""
+    }]
+}
+
+
+def obtener_matricula(request):
+    headers = request.split('\r\n')
+    if headers[0].startswith('POST'):
+        matricula = " ".join(headers[0].split("="))
+        matricula = matricula.split(" ")[2]
+        
+
+        # Enviar una respuesta al cliente
+        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nPetición POST recibida'
+        client_socket.sendall(response.encode())
+    else:
+        response = 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\nMétodo no permitido'
+        client_socket.sendall(response.encode())
+
+    return matricula
+
+if __name__ == "__main__":
+
+
+    # Configuración del servidor
+    HOST = socket.getfqdn() # Dirección IP del servidor (localhost)
+    IP_LOCAL =  socket.gethostbyname_ex(HOST)[2][1]
+    PORT = 8080         # Puerto del servidor
+    # Crear un socket TCP/IP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((IP_LOCAL, PORT))  # Vincular el socket a la dirección y puerto
+        server_socket.listen()            # Poner el socket en modo de escucha
+
+        print(f"Servidor escuchando en {IP_LOCAL}:{PORT}")
+
+        while True:
+            # Esperar a que un cliente se conecte
+            client_socket, client_address = server_socket.accept()
+            with client_socket:
+                print(f"Conexión establecida con {client_address}")
+                request = client_socket.recv(1024).decode()
+                print(obtener_matricula(request))
+                break
